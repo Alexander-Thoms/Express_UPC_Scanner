0cfcad061f5a0b2de9e7c0a81a8beb5ffcf275ad
Alexander-Thoms <alex.g.thoms@gmail.com>
2026-04-15 15:19:44 +0000

    fixed SQL injection & information hiding issues
    fixed information hiding and sql injections using (?) instead

--- index.js
+++ index.js
@@ -1,36 +1,187 @@
 const port = 8080;
 const express = require('express');
 const app = express();
+app.disable('x-powered-by'); // information exporure disable x powered by
+const mysql = require('mysql2');
+/**
+ 
+
+ create table items (
+ id int auto_increment primary key,
+ productnames TEXT NOT NULL
+ );
+
+
+
+ */
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',// hard coded passwords unsafe use env
+    database: 'shoppingdatabase'
+
+});
+
+connection.connect((err) =>{
+    if (err){
+        console.error("error connecting to myql: " + err.stack)
+    }
+    console.log("connected to sql");
+
+});
+
 
 
 
 app.use(express.urlencoded({extended: true}));
+
+ function makelist(namesparam){
+        let content = ""
+        let start = "<li>" // html tags
+        let end = "</li>"
+        for(let index = 0; index < namesparam.length; index++){ // for loop each items in names array
+            content += start + namesparam[index] + end // create proper li html code
+        }
+        return content // return the html
+      
+    }
+
+     function deleteitem(deleteparam){
+         let deletecode = `delete from productnames where ("${deleteparam}")`
+        connection.query(deletecode, (error, results, fields)=>{
+            if(error){
+                warn(error)
+            }
+            console.log(results)
+        });
+        return console.log("delete success");
+    }
+
+
+
+
+
 app.get('/', (req,res)=>{
-    res.send(`
+
+
+
+ let showallcode = `select productnames from items` // select all product names from items
+    let names = [];
+    connection.query(showallcode, (error, results, fields)=>{
+         if(error){
+            console.warn(error)
+        }
+         results.forEach((record)=>{ // for reach record it finds push it into names array
+            names.push(record.productnames);
+        });
+    console.log("main logs: ");
+    console.log(names);
+    console.log(names.length)
+
+      res.send(`
         <html>
         <head>
-        <title>upc lookup</title>
+        <title>shopping list main page</title>
         </head>
-        <body>
-        <h1>upc lookup</h1>
+       <body>
+       <h1>Shopping List</h1>
         <form method="POST" action="/lookup">
         <label>
-        <input type="text" name="upc" value="049000000443">
+        <input type="text" name="upc" value="049000061017">
         </label>
         <button type="submit">look up</button>
         </form>
+        <form method="POST" action ="/namelookup">
+        <input type="text" name="productnames" value="product name"> 
+        <button type="submit">add non upc product</button>
+        </form>
+        <ol>
+        ${makelist(names)}
+        </ol>
         </body>
         </html>
 `);
+
+
+
+
+    });
+
+      
+
+});
+app.post(`/namelookup`, async(req, res)=>{
+    let input = req.body.productnames
+    let insertcodename =  `insert into items(productnames) values (?)`
+    connection.query(insertcodename, [input], (error, results, field)=>{
+        if(error){
+            warn(error)
+        }
+        console.log(results);
+        console.log("inserted name success");
+
+    });
+
+     function makelist(namesparam){
+        let content = ""
+        let start = "<li>" // html tags
+        let end = "</li>"
+        for(let index = 0; index < namesparam.length; index++){ // for loop each items in names array
+            content += start + namesparam[index] + end // create proper li html code
+        }
+        return content // return the html
+      
+    }
+
+    let showallcode = `select productnames from items` // select all product names from items
+    let names = [];
+    connection.query(showallcode, (error, results, fields)=>{
+         if(error){
+            console.warn(error)
+        }
+         results.forEach((record)=>{ // for reach record it finds push it into names array
+            names.push(record.productnames);
+        });
+    console.log(names);
+
+
+
+     res.send(`
+
+    <html>
+    <body>
+    <h1>Shopping List</h1>
+        <form method="POST" action="/lookup">
+        <label>
+        <input type="text" name="upc" value="049000061017">
+        </label>
+        <button type="submit">look up</button>
+        </form>
+        <form method="POST" action= "/namelookup">
+        <input type="text" name="productnames" value="product name"> 
+        <button type="submit">add non upc product</button>
+        </form>
+    <ol>
+    ${makelist(names)}
+    </ol>
+    </html>
+
+
+    `);
+});
+
+
 });
 app.post('/lookup', async(req,res)=>{
     const upc = req.body.upc;
-    apikey = "";
+    
     const url = `https://barcode-spider.p.rapidapi.com/v1/lookup?upc=${upc}`;
     const options = {
   method: 'GET',
   headers: {
-    'x-rapidapi-key': '',
+    'x-rapidapi-key': 'effcf28ec4msh408c10f13a68dcdp1836b1jsnc602376e95f5',
     'x-rapidapi-host': 'barcode-spider.p.rapidapi.com'
   }
 };
@@ -42,23 +193,75 @@ app.post('/lookup', async(req,res)=>{
     const title = data.item_attributes.title;
     const image = data.item_attributes.image;
     const store = data.Stores?.[0]?.link;
+    const upccode = data.item_attributes.upc;
+    let input = upc.value;
 
-    console.log(data)
+    function makelist(namesparam){
+        let content = ""
+        let start = "<li>" // html tags
+        let end = "</li>"
+        for(let index = 0; index < namesparam.length; index++){ // for loop each items in names array
+            content += start + namesparam[index] + end // create proper li html code
+        }
+        return content // return the html
+      
+    }
 
+   
+   
 
-res.send(`
+    console.log(data);
+    insertcode = `insert into items(productnames) values (?)`
+    connection.query(insertcode, [title], (error, results, fields)=>{
+        if(error){
+            console.warn(error)
+        }
+        console.log(results)
+        console.log("inserted success"); // if everything worked out it will
+        
+    });
+    let showallcode = `select productnames from items` // select all product names from items
+    let names = [];
+    connection.query(showallcode, (error, results, fields)=>{
+         if(error){
+            console.warn(error)
+        }
+         results.forEach((record)=>{ // for reach record it finds push it into names array
+            names.push(record.productnames);
+        });
+    console.log(names);
+
+     res.send(`
+
     <html>
     <body>
-    <h1>${title}</h1>
-    <img src="${image}"width=100px height=100px>
-    <br>
-    <a href="${store}">amazon link</a>
-    </body>
+    <h1>Shopping List</h1>
+        <form method="POST" action="/lookup">
+        <label>
+        <input type="text" name="upc" value="049000061017">
+        </label>
+        <button type="submit">look up</button>
+        </form>
+        <form method="POST" action="/namelookup">
+        <input type="text" name="productnames" value="product name"> 
+        <button type="submit">add non upc product</button>
+        </form>
+    <ol>
+    ${makelist(names)}
+    </ol>
     </html>
+
+
     `);
+   
   
 	
 });
+    });
+  
+   
+
+
 
 
 
